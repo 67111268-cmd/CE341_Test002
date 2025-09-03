acf4a40d7cc87ac71615dc397b58f4c8072bf735
67111268-cmd <67111268@dpu.ac.th>
2025-09-03 13:08:02 +0700

    asd

---
+++ myfunc.py
@@ -0,0 +1,10 @@
+def add_numbers(A, B):
+    """
+    ฟังก์ชันสำหรับบวกเลข A + B
+    รับค่า A และ B แล้วคืนค่าผลลัพธ์
+    """
+    return A + B
+
+
+result = add_numbers(5, 7)
+print("ผลลัพธ์ของ A + B คือ:", result)
